7b365c0625791cfd912fa761f0da9296f52f5539
nikname-1337 <nikhil.kalele9@gmail.com>
2022-05-28 16:38:15 +1000

    added components and css for board/keyboard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-
+import Keyboard from './components/Keyboard';
+import Board from './components/Board';
 import './App.css';
+import { boardDefault } from './Words';
+import {useState, createContext } from 'react';
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault);
   return (
     <div className="App">
       <nav>
         <h1>Wordle</h1>
       </nav>
-
-
+      <AppContext.Provider value={{ board, setBoard }}>
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
     </div>
   );
 }
---
+++ src/components/Letter.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import { AppContext } from '../App'
+
+
+
+
+function Letter({ letterPosition, attemptValue }) {
+    const { board } = useContext(AppContext);
+    const letter = board[attemptValue][letterPosition];
+    return (
+        <div className='letter'> {letter}</div>
+    )
+}
+
+export default Letter
