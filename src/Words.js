1edd6f886c3afffe6c33e73c95820371e1151277
nikname-1337 <nikhil.kalele9@gmail.com>
2022-06-05 15:37:51 +1000

    working

--- src/Words.js
+++ src/Words.js
@@ -1,4 +1,5 @@
 import wordBank from "./wordle-bank.txt";
+import guessBank from "./wordle-bank-guess.txt";
 
 export const boardDefault = [
   ["", "", "", "", ""],
@@ -10,15 +11,21 @@ export const boardDefault = [
 ];
 
 export const generateWordSet = async () => {
-  let wordSet;
+  let wordSet = new Set();
   let todaysWord;
   await fetch(wordBank)
     .then((response) => response.text())
     .then((result) => {
       const wordArray = result.split("\n");
-      todaysWord = wordArray[Math.floor(Math.random()*wordArray.length)];
-      wordSet = new Set(wordArray);
+      todaysWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+      wordArray.forEach(wordSet.add, wordSet);
+    });
+  await fetch(guessBank)
+    .then((response) => response.text())
+    .then((result) => {
+      const wordArray = result.split("\n");
+      wordArray.forEach(wordSet.add, wordSet);
     });
 
-  return { wordSet, todaysWord};
+  return { wordSet, todaysWord };
 };
--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,19 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../App";
 
 function GameOver() {
-  const { gameOver, setGameOver, correctWord, curAttempt } =
+  const DICTIONARY_API_BASE_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+  const [definition, setDefinition] = useState("Not Found");
+  const { gameOver, correctWord, curAttempt } =
     useContext(AppContext);
+  
+  const fetchWordDefinitions = async word => {
+    console.log(`Making request for definitions of ${word}...`)
+    const response = await fetch(DICTIONARY_API_BASE_URL + word);
+    const json = await response.json();
+    return json[0].meanings.flatMap(m => m.definitions).flatMap(d => d.definition);
+  };
+  const getWordDefinitions = () => {
+    fetchWordDefinitions(correctWord).then((meanings) => {
+      setDefinition(meanings[0]);
+    })
+};
+  useEffect(() => getWordDefinitions());
   return (
     <div className="gameOver">
       <h3>
         {" "}
         {gameOver.guessedWord ? "You Correctly Guessed" : "You Failed :/"}
       </h3>
-      <h1>Correct: {correctWord}</h1>
+      <h2>Correct: {correctWord.toUpperCase()}</h2>
       {gameOver.guessedWord && (
         <h3> You guessed in {curAttempt.attempt} attempts</h3>
       )}
+      <h3>{definition}</h3>
     </div>
   );
 }
