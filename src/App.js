35902b714e43fdb4bf664f10e37684df7f0612df
nikname-1337 <nikhil.kalele9@gmail.com>
2022-06-05 18:45:54 +1000

    done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import { boardDefault, generateWordSet } from "./Words";
 import { useState, createContext, useEffect } from "react";
 import GameOver from "./components/GameOver";
+import { letterState } from "./components/Letter";
 
 export const AppContext = createContext();
 
@@ -20,15 +21,50 @@ function App() {
     guessedWord: false,
   });
   const [correctWord, setCorrectWord] = useState("");
-  
+  const [finStates, setFinStates] = useState([
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+  ]);
+
   useEffect(() => {
     generateWordSet().then((words) => {
       // console.log(words);
       setWordSet(words.wordSet);
       setCorrectWord(words.todaysWord);
+
     });
   }, []);
 
+  const checkGuess = ((guessedWord) => {
+    var word = correctWord.split('');
+    var states = ["","","","",""];
+    var count = {};
+    word.forEach(val => count[val] = (count[val] || 0) + 1);
+    
+    for (let i = 0; i < 5; i++){      
+      if (guessedWord[i] === correctWord[i]){
+        states[i] = (letterState.correct);
+        count[guessedWord[i]] -= 1;
+      } 
+    }
+    // console.log(count);
+    for (let i = 0; i < 5; i++){
+      if (correctWord.includes(guessedWord[i]) && count[guessedWord[i]] > 0){
+        states[i] = (letterState.almost);
+        count[guessedWord[i]] -= 1;
+      }else if (states[i] !== letterState.correct){
+        states[i] = (letterState.error);
+      }
+    }
+    // console.log(states);
+    const newStates = [...finStates];
+    newStates[curAttempt.attempt] = states;
+    setFinStates(newStates);
+  });
 
   const onSelectLetter = (keyVal) => {
     if (curAttempt.position > 4 || curAttempt.attempt > 5) return;
@@ -45,7 +81,7 @@ function App() {
     setBoard(newBoard);
     setCurAttempt({ ...curAttempt, position: curAttempt.position - 1 });
   };
-
+  
   const onEnter = () => {
     if (curAttempt.position !== 5 || curAttempt.attempt > 5) return;
 
@@ -53,11 +89,13 @@ function App() {
     for (let i = 0; i < 5; i++) {
       curWord += board[curAttempt.attempt][i].toLowerCase();
     }
-
+    
     if (wordSet.has(curWord.toLowerCase())) {
       setCurAttempt({ attempt: curAttempt.attempt + 1, position: 0 });
+      checkGuess(curWord);
     } else {
       alert("Word Not Found!");
+      return;
     }
     if (curWord === correctWord) {
       setGameOver({ gameOver: true, guessedWord: true });
@@ -87,6 +125,7 @@ function App() {
           setDisabledLetters,
           gameOver,
           setGameOver,
+          finStates,
         }}
       >
         <div className="game">
--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,21 +1,30 @@
 import React, { useContext, useEffect } from "react";
 import { AppContext } from "../App";
 
-function Letter({ letterPosition, attemptValue, occ }) {
-  const { board, correctWord, curAttempt, setDisabledLetters } =
-    useContext(AppContext);
-    
-  const letter = board[attemptValue][letterPosition];
+export const letterState = {
+  correct: "correct",
+  almost: "almost",
+  error: "error",
+};
 
+function Letter({ letterPosition, attemptValue }) {
+  const { board, correctWord, curAttempt, setDisabledLetters, finStates } =
+    useContext(AppContext);
+  const letter = board[attemptValue][letterPosition];
   const correct = correctWord[letterPosition] === letter.toLowerCase();
   const almost =
     !correct &&
     letter !== "" &&
     correctWord.toLowerCase().includes(letter.toLowerCase());
 
+  // const letterState =
+  //   curAttempt.attempt > attemptValue &&
+  //   (correct ? "correct" : almost ? "almost" : "error");
+  // console.log(finStates);
   const letterState =
-    curAttempt.attempt > attemptValue &&
-    (correct ? "correct" : almost ? "almost" : "error");
+    curAttempt.attempt > attemptValue
+      ? finStates[attemptValue][letterPosition]
+      : "";
 
   useEffect(() => {
     if (letter !== "" && !correct && !almost) {
